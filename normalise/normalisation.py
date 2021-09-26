991db0e1ba094d0ab992f23081ba67c513d9f357
dat127 <datthanhnguyen@vulcanlabs.co>
2021-09-26 17:07:12 +0000

    separeting quotes and double quotes sentences into tokens

--- normalise/normalisation.py
+++ normalise/normalisation.py
@@ -109,6 +109,13 @@ def tokenize_basic(text):
                 out.extend([guess[i][:-1], guess[i][-1]])
         elif guess[i][-1] in ['!', '?'] and guess[i][:-1].isalpha():
             out.extend([guess[i][:-1], guess[i][-1]])
+        elif guess[i][0] in ['"', "'"]:
+            if guess[i][-1] in ['"', "'"]:
+                out.extend([guess[i][0], guess[i][1:-1], guess[i][-1]])
+            else:
+                out.extend([guess[i][0], guess[i][1:]])
+        elif guess[i][-1] in ['"', "'"]:
+                out.extend([guess[i][:-1], guess[i][-1]])
         elif guess[i][-1] == '.' and guess[i][:-1].isalpha():
             following = guess[i + 1]
             if following.istitle() and following.lower() in wordlist:
